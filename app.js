df7eed92de5f0a14a1ab59e7df278eec20f34542
Shreya Maheshwari <shreyam803@gmail.com>
2021-04-12 22:12:06 +0530

    modified dotenv file

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const User = require("./models/user")
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 app.set('view-engine', 'ejs');
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json());
--- db/db.js
+++ db/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const MONGO_URL = 'mongodb://127.0.0.1:27017/login-api'
+const MONGO_URL = process.env.MONGO_URL_DEV;
 
 mongoose.connect(MONGO_URL, {
     useUnifiedTopology: true,
